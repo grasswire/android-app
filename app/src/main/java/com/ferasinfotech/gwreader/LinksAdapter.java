6d9989a497d118adebff13dac73570a5eb16464a
John Feras <jferas@ferasinfotech.com>
2015-11-30 09:21:59 -0500

    in the middle of adding link detailed data

--- app/src/main/java/com/ferasinfotech/gwreader/LinksAdapter.java
+++ app/src/main/java/com/ferasinfotech/gwreader/LinksAdapter.java
@@ -29,6 +29,9 @@ public class LinksAdapter extends BaseAdapter implements OnClickListener
     private static final String TAG_LINK_DATA = "linkData";
     private static final String TAG_LINK_TITLE = "title";
     private static final String TAG_LINK_DESCRIPTION = "description";
+    private static final String TAG_LINK_TYPE = "type";
+    private static final String TAG_LINK_TWEET = "tweet";
+    private static final String TAG_TWEET_TEXT = "text";
 
     private Activity activity;
     private JSONArray links;
@@ -79,12 +82,47 @@ public class LinksAdapter extends BaseAdapter implements OnClickListener
     }
 
     /********* Create a holder Class to contain inflated xml file elements *********/
-    public static class ViewHolder{
+    public class ViewHolder{
 
         public TextView title;
         public TextView description;
-        //public ImageView image;
+        public ImageView image;
+        public ImageView profile_image;
+        public TextView profile_name;
+        public TextView elapsed_time;
 
+        public ViewHolder create(View vi)
+        {
+            ViewHolder holder;
+
+            holder = new ViewHolder();
+            holder.profile_image = (ImageView) vi.findViewById(R.id.link_profile_image);
+            holder.profile_name = (TextView) vi.findViewById(R.id.link_profile_name);
+            holder.elapsed_time = (TextView) vi.findViewById(R.id.link_elapsed_time);
+            holder.title = (TextView) vi.findViewById(R.id.link_title);
+            holder.image = (ImageView) vi.findViewById(R.id.link_image);
+            holder.description = (TextView)vi.findViewById(R.id.link_description);
+
+            return holder;
+        }
+    }
+
+    private void handle_link_parse_exception()
+    {
+        Log.d("***DEBUG***", "JSON parse error on links");
+    }
+
+    private void do_tweet_link(JSONObject link, ViewHolder holder)
+    {
+        try
+        {
+            JSONObject tw = link.getJSONObject(TAG_LINK_TWEET);
+            holder.description.setText(tw.getString(TAG_TWEET_TEXT));
+        }
+        catch (JSONException e)
+        {
+            handle_link_parse_exception();
+        }
     }
 
     public View getView(int position, View convertView, ViewGroup parent)
@@ -95,20 +133,12 @@ public class LinksAdapter extends BaseAdapter implements OnClickListener
         if (vi == null)
         {
             vi = inflater.inflate(R.layout.list_item, null);
-
-            /****** View Holder Object to contain tabitem.xml file elements ******/
-
-            holder = new ViewHolder();
-            holder.title = (TextView) vi.findViewById(R.id.link_title);
-            holder.description = (TextView)vi.findViewById(R.id.link_description);
-            //holder.image=(ImageView)vi.findViewById(R.id.image);
-
-            /************  Set holder with LayoutInflater ************/
-            vi.setTag( holder );
+            holder = new ViewHolder().create(vi);
+            vi.setTag(holder);
         }
         else
         {
-            holder=(ViewHolder)vi.getTag();
+            holder = (ViewHolder) vi.getTag();
         }
 
         if (links.length() == 0)
@@ -118,18 +148,36 @@ public class LinksAdapter extends BaseAdapter implements OnClickListener
         }
         else
         {
+            String title = "Missing Title";
+            String description = "Missing Description";
             try
             {
                 JSONObject link = links.getJSONObject(position);
-                JSONObject link_data = link.getJSONObject(TAG_LINK_DATA);
-                holder.title.setText(link_data.getString(TAG_LINK_TITLE));
-                holder.description.setText(link_data.getString(TAG_LINK_DESCRIPTION));
+                String link_type = link.getString(TAG_LINK_TYPE);
+                if (link_type.contains("TweetLinkJsonModel"))
+                {
+                    do_tweet_link(link, holder);
+                }
+                else if (link_type.contains("PlainLinkJsonModel"))
+                {
+                    JSONObject link_data = link.getJSONObject(TAG_LINK_DATA);
+
+                    title = link_data.getString(TAG_LINK_TITLE);
+                    description = link_data.getString(TAG_LINK_DESCRIPTION);
+                }
+                else if (link_type.contains("VideoLinkJsonModel"))
+                {
+                    JSONObject link_data = link.getJSONObject(TAG_LINK_DATA);
+
+                    description = link_data.getString(TAG_LINK_DESCRIPTION);
+                    title = "";
+                }
             }
             catch (JSONException e)
             {
-                holder.title.setText("Link Error");
-                holder.description.setText("");
+                handle_link_parse_exception();
             }
+            holder.title.setText(title);
         }
         return vi;
     }
